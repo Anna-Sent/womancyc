09bbe62a0724a6fe9c84d53fbc06024117b63a96
anna sent <anna.sent@gmail.com>
2013-05-15 13:38:38 +0400

    date picker dialog

--- src/com/anna/sent/soft/womancyc/fragments/DatePickerFragment.java
+++ src/com/anna/sent/soft/womancyc/fragments/DatePickerFragment.java
@@ -1,6 +1,5 @@
 package com.anna.sent.soft.womancyc.fragments;
 
-import java.lang.reflect.Field;
 import java.util.Calendar;
 
 import android.app.Activity;
@@ -10,14 +9,12 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.widget.DatePicker;
 
 import com.anna.sent.soft.womancyc.R;
 import com.anna.sent.soft.womancyc.shared.Shared;
 
 public class DatePickerFragment extends DialogFragment implements
 		DialogInterface.OnClickListener {
-	private DatePicker mDatePicker = null;
 	private OnDateSetListener mListener = null;
 
 	@Override
@@ -31,20 +28,6 @@ public class DatePickerFragment extends DialogFragment implements
 				mListener, year, month, day);
 		dialog.setButton(DialogInterface.BUTTON_NEUTRAL,
 				getString(R.string.today), this);
-
-		try {
-			Field field = DatePickerDialog.class
-					.getDeclaredField("mDatePicker");
-			field.setAccessible(true);
-			mDatePicker = (DatePicker) field.get(dialog);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (NoSuchFieldException e) {
-			e.printStackTrace();
-		}
-
 		return dialog;
 	}
 
@@ -62,9 +45,12 @@ public class DatePickerFragment extends DialogFragment implements
 
 	@Override
 	public void onClick(DialogInterface dialog, int which) {
-		if (which == DialogInterface.BUTTON_NEUTRAL && mDatePicker != null) {
-			mListener.onDateSet(mDatePicker, mDatePicker.getYear(),
-					mDatePicker.getMonth(), mDatePicker.getDayOfMonth());
+		if (which == DialogInterface.BUTTON_NEUTRAL) {
+			Calendar today = Calendar.getInstance();
+			int year = today.get(Calendar.YEAR);
+			int month = today.get(Calendar.MONTH);
+			int day = today.get(Calendar.DAY_OF_MONTH);
+			mListener.onDateSet(null, year, month, day);
 		}
 	}
 }
