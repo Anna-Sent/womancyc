55d75ac88b1ac08720b5677ade81482eeba42168
anna sent <anna.sent@gmail.com>
2013-05-15 13:09:22 +0400

    date picker dialog hack

--- src/com/anna/sent/soft/womancyc/DatePickerFragment.java
+++ src/com/anna/sent/soft/womancyc/DatePickerFragment.java
@@ -1,18 +1,23 @@
 package com.anna.sent.soft.womancyc;
 
+import java.lang.reflect.Field;
 import java.util.Calendar;
-
-import com.anna.sent.soft.womancyc.shared.Shared;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
 
-public class DatePickerFragment extends DialogFragment {
-	OnDateSetListener mListener;
+import com.anna.sent.soft.womancyc.shared.Shared;
+
+public class DatePickerFragment extends DialogFragment implements
+		DialogInterface.OnClickListener {
+	private DatePicker mDatePicker = null;
+	private OnDateSetListener mListener = null;
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -21,7 +26,25 @@ public class DatePickerFragment extends DialogFragment {
 		int year = c.get(Calendar.YEAR);
 		int month = c.get(Calendar.MONTH);
 		int day = c.get(Calendar.DAY_OF_MONTH);
-		return new DatePickerDialog(getActivity(), mListener, year, month, day);
+		DatePickerDialog dialog = new DatePickerDialog(getActivity(),
+				mListener, year, month, day);
+		dialog.setButton(DialogInterface.BUTTON_NEUTRAL,
+				getString(R.string.today), this);
+
+		try {
+			Field field = DatePickerDialog.class
+					.getDeclaredField("mDatePicker");
+			field.setAccessible(true);
+			mDatePicker = (DatePicker) field.get(dialog);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+
+		return dialog;
 	}
 
 	@Override
@@ -33,6 +56,14 @@ public class DatePickerFragment extends DialogFragment {
 			throw new ClassCastException(activity.toString()
 					+ " must implement "
 					+ OnDateSetListener.class.getSimpleName());
+		}
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		if (which == DialogInterface.BUTTON_NEUTRAL && mDatePicker != null) {
+			mListener.onDateSet(mDatePicker, mDatePicker.getYear(),
+					mDatePicker.getMonth(), mDatePicker.getDayOfMonth());
 		}
 	}
 }
