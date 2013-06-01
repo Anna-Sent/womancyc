121a4d3a29669e5775c2db585d6e2ee73cb278eb
anna sent <anna.sent@gmail.com>
2013-06-02 02:49:31 +0400

    debug

--- src/com/anna/sent/soft/womancyc/superclasses/StateSaverActivity.java
+++ src/com/anna/sent/soft/womancyc/superclasses/StateSaverActivity.java
@@ -12,7 +12,7 @@ import android.util.Log;
 public abstract class StateSaverActivity extends FragmentActivity implements
 		StateSaver {
 	private static final String TAG = "moo";
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 
 	private String wrapMsg(String msg) {
 		return getClass().getSimpleName() + ": " + msg;
