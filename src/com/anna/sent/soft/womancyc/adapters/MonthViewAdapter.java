6d7842c24eb79d86c553e49082fea92ef9ff2ef1
anna sent <anna.sent@gmail.com>
2013-06-02 02:50:18 +0400

    debug

--- src/com/anna/sent/soft/womancyc/adapters/MonthViewAdapter.java
+++ src/com/anna/sent/soft/womancyc/adapters/MonthViewAdapter.java
@@ -26,7 +26,7 @@ import com.anna.sent.soft.womancyc.utils.ThemeUtils;
 
 public class MonthViewAdapter extends BaseAdapter {
 	private static final String TAG = "moo";
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 
 	private String wrapMsg(String msg) {
 		return getClass().getSimpleName() + ": " + msg;
