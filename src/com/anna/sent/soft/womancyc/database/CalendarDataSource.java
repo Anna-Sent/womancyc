4df55ff9a25f6b94d65395b11d8cee84d486668c
anna sent <anna.sent@gmail.com>
2013-07-04 19:04:27 +0400

    debug to false

--- src/com/anna/sent/soft/womancyc/database/CalendarDataSource.java
+++ src/com/anna/sent/soft/womancyc/database/CalendarDataSource.java
@@ -14,7 +14,7 @@ import com.anna.sent.soft.womancyc.data.CalendarData;
 
 public class CalendarDataSource {
 	private static final String TAG = "moo";
-	private static final boolean DEBUG_NOTES = true;
+	private static final boolean DEBUG_NOTES = false;
 	private static final boolean DEBUG_ROWS = false;
 	private static final boolean DEBUG_CRUD = false;
 
