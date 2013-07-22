1e2562206d6d400b587e81d5833a02b6f337a526
anna sent <anna.sent@gmail.com>
2013-07-23 03:14:42 +0400

    debug to false

--- src/com/anna/sent/soft/womancyc/fragments/MonthViewFragment.java
+++ src/com/anna/sent/soft/womancyc/fragments/MonthViewFragment.java
@@ -23,7 +23,7 @@ import com.anna.sent.soft.womancyc.utils.OnSwipeTouchListener;
 public class MonthViewFragment extends Fragment implements
 		MonthViewAdapter.Listener, OnClickListener, DataKeeperClient {
 	private static final String TAG = "moo";
-	private static final boolean DEBUG = true;
+	private static final boolean DEBUG = false;
 
 	private String wrapMsg(String msg) {
 		return getClass().getSimpleName() + ": " + msg;
