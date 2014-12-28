f17d9a05fe48d9e8047e26107b7c314123638402
anna sent <anna.sent@gmail.com>
2014-12-28 16:17:13 +0400

    ECLAIR Api level

--- src/com/anna/sent/soft/womancyc/utils/UserEmailFetcher.java
+++ src/com/anna/sent/soft/womancyc/utils/UserEmailFetcher.java
@@ -2,15 +2,12 @@ package com.anna.sent.soft.womancyc.utils;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 
 /**
  * This class uses the AccountManager to get the primary email address of the
  * current user.
  */
-@TargetApi(Build.VERSION_CODES.ECLAIR)
 public class UserEmailFetcher {
 	private static final String INVALID_EMAIL = "";
 
@@ -20,17 +17,13 @@ public class UserEmailFetcher {
 	 * @return email or empty string
 	 */
 	public static String getEmail(Context context) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
-			AccountManager accountManager = AccountManager.get(context);
-			Account account = getAccount(accountManager);
+		AccountManager accountManager = AccountManager.get(context);
+		Account account = getAccount(accountManager);
 
-			if (account == null) {
-				return INVALID_EMAIL;
-			} else {
-				return account.name;
-			}
-		} else {
+		if (account == null) {
 			return INVALID_EMAIL;
+		} else {
+			return account.name;
 		}
 	}
 
