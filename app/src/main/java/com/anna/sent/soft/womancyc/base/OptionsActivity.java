51da5ca8a0a23fddd3c9929691d57823e220fb1b
Anna-Sentyakova <anna.sent@gmail.com>
2017-02-12 15:44:34 +0400

    runtime permissions for pre-M

--- app/src/main/java/com/anna/sent/soft/womancyc/base/OptionsActivity.java
+++ app/src/main/java/com/anna/sent/soft/womancyc/base/OptionsActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -117,6 +118,11 @@ public abstract class OptionsActivity extends DataKeeperActivity {
                 break;
             default:
                 return;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            doAction(requestCode);
+            return;
         }
 
         if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
